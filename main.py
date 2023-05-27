480e36bb90572c504e2761b14eb30e79e992e185
Tobin Rosenau <tobin.rosenau@mailbox.org>
2023-05-27 12:11:34 +0000

    add more detail to output

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ import json
 from PyViCare.PyViCare import PyViCare
 
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read("config.ini")
 
-client_id = config.get('vicare', 'client_id')
-email = config.get('vicare', 'email')
-password = config.get('vicare', 'password')
+client_id = config.get("vicare", "client_id")
+email = config.get("vicare", "email")
+password = config.get("vicare", "password")
 
 vicare = PyViCare()
 vicare.initWithCredentials(email, password, client_id, "token.save")
 for x in vicare.devices:
-  print(x.getModel())
-  if(x.getModel() == "E3_HEMS"):
-    device = x
-    print("Device Model: " + device.getModel())
-    print("Status: " + ("Online" if device.isOnline() else "Offline"))
-    t = device.asAutoDetectDevice()
-    data = {"transfer_power_exchange": t.getTransferPowerExchange}
-    print(json.dumps(data))
-
-
+    if x.getModel() == "E3_HEMS":
+        device = x
+        t = device.asAutoDetectDevice()
+        current_production = t.getPhotovoltaicProductionCurrent()
+        if(t.getPhotovoltaicProductionCurrentUnit() == "kilowatt"):
+            current_production = int(current_production*1000) #convert kW to W
+        
+        data = {
+            "transfer_power_exchange": t.getPointOfCommonCouplingTransferPowerExchange(),
+            "pv-status": t.getPhotovoltaicStatus(),
+            "pv-production": current_production,
+        }
+        print(json.dumps(data))

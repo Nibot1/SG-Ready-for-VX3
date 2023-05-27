2cea5fc35b7fc638a399ff166c0de13304ed44c1
Tobin Rosenau <tobin.rosenau@mailbox.org>
2023-05-27 19:06:23 +0000

    add ees current power

--- main.py
+++ main.py
@@ -18,12 +18,16 @@ for x in vicare.devices:
         device = x
         t = device.asAutoDetectDevice()
         current_production = t.getPhotovoltaicProductionCurrent()
-        if(t.getPhotovoltaicProductionCurrentUnit() == "kilowatt"):
-            current_production = int(current_production*1000) #convert kW to W
-        
+        ees_current_power = t.getElectricalEnergySystemPower()
+        if t.getPhotovoltaicProductionCurrentUnit() == "kilowatt":
+            current_production = int(current_production * 1000)  # convert kW to W
+        if t.getElectricalEnergySystemPowerUnit() == "kilowatt":
+            ees_current_power = int(ees_current_power * 1000)  # convert kW to W
+
         data = {
             "transfer_power_exchange": t.getPointOfCommonCouplingTransferPowerExchange(),
             "pv_status": t.getPhotovoltaicStatus(),
             "pv_production": current_production,
+            "ees_current_power": ees_current_power,
         }
         print(json.dumps(data))

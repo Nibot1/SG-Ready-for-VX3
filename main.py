ec80e25d08a3fe477ec21bab1daa56d1a9ad62fb
Tobin Rosenau <tobin.rosenau@mailbox.org>
2023-10-14 10:46:00 +0200

    Add DWH-Storage-Temp Request

--- main.py
+++ main.py
@@ -14,25 +14,34 @@ password = config.get("vicare", "password")
 try:
     vicare = PyViCare()
     vicare.initWithCredentials(email, password, client_id, "token.save")
-    for x in vicare.devices:
-        if x.getModel() == "E3_VitoCharge_03":
-            device = x
-            t = device.asAutoDetectDevice()
-            current_production = t.getPhotovoltaicProductionCurrent()
-            ees_current_power = t.getElectricalEnergySystemPower()
-            if t.getPhotovoltaicProductionCurrentUnit() == "kilowatt":
-                current_production = int(current_production * 1000)  # convert kW to W
-            if t.getElectricalEnergySystemPowerUnit() == "kilowatt":
-                ees_current_power = int(ees_current_power * 1000)  # convert kW to W
-
+    if sys.argv[0] == dhw-temperature:
+            device = vicare.devices[0]
+            t= device.asAutoDetectDevice()
             data = {
-                "result": "ok",
-                "transfer_power_exchange": t.getPointOfCommonCouplingTransferPowerExchange(),
-                "pv_status": t.getPhotovoltaicStatus(),
-                "pv_production": current_production,
-                "ees_current_discharge": ees_current_power,
+                "result":"ok,
+                "dhw-storgae-temp": t.getDomesticHotWaterStorageTemperature()
             }
             print(json.dumps(data))
+    else:
+        for x in vicare.devices:
+            if x.getModel() == "E3_VitoCharge_03":
+                device = x
+                t = device.asAutoDetectDevice()
+                current_production = t.getPhotovoltaicProductionCurrent()
+                ees_current_power = t.getElectricalEnergySystemPower()
+                if t.getPhotovoltaicProductionCurrentUnit() == "kilowatt":
+                    current_production = int(current_production * 1000)  # convert kW to W
+                if t.getElectricalEnergySystemPowerUnit() == "kilowatt":
+                    ees_current_power = int(ees_current_power * 1000)  # convert kW to W
+
+                data = {
+                    "result": "ok",
+                    "transfer_power_exchange": t.getPointOfCommonCouplingTransferPowerExchange(),
+                    "pv_status": t.getPhotovoltaicStatus(),
+                    "pv_production": current_production,
+                    "ees_current_discharge": ees_current_power,
+                }
+                print(json.dumps(data))
 except:
     output = { "result": "error" }
     print(json.dumps(output))
